ae8048028d3649fc5fe8f928f8a4584fcc191c29
Man Alexandra <alexandra.man@student.unitbv.ro>
2025-06-06 12:22:37 +0300

    recipes table

--- src/app/features/customer-recipes/customer-recipes.component.ts
+++ src/app/features/customer-recipes/customer-recipes.component.ts
@@ -9,6 +9,8 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Recipe } from '../../models/recipe-item.model'; 
 import { RECIPES } from '../../mocked/recipes-data.mock';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+
 
 
 @Component({
@@ -23,13 +25,16 @@ import { RECIPES } from '../../mocked/recipes-data.mock';
     NzModalModule,
     NzFormModule,
     NzInputModule,
-    NzIconModule
+    NzIconModule,
+    NzPaginationModule
   ],
   templateUrl: './customer-recipes.component.html',
   styleUrls: ['./customer-recipes.component.css']
 })
 export class CustomerRecipesComponent implements OnInit{
   recipes: Recipe[] = [];
+  currentPage = 1;
+  pageSize = 8;
 
   ngOnInit(): void {
     this.recipes = RECIPES;
@@ -42,5 +47,13 @@ export class CustomerRecipesComponent implements OnInit{
   openEditModal(recipe: Recipe) {
     // vei completa asta ulterior
   }
+  get pagedRecipes(): Recipe[] {
+  const start = (this.currentPage - 1) * this.pageSize;
+  return this.recipes.slice(start, start + this.pageSize);
+}
+
+onPageChange(page: number): void {
+  this.currentPage = page;
+}
 
 }
